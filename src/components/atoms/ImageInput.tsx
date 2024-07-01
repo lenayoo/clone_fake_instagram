2de50e9db862ba47d23bdcf870c24662910de93f
lenayoo <lenayoo17@gmail.com>
2024-07-01 19:29:35 +0900

    add image

--- src/components/atoms/ImageInput.tsx
+++ src/components/atoms/ImageInput.tsx
@@ -1,9 +1,11 @@
 import { Input } from "@mui/material";
 
 export const ImageInput = ({
+  value,
   imageChangeHandler
 }: {
+  value: FileList | null;
   imageChangeHandler: React.ChangeEventHandler;
 }) => {
-  return <Input type="file" onChange={imageChangeHandler} />;
+  return <Input type="file" onChange={imageChangeHandler} value={value} />;
 };
--- src/components/molecules/Form.tsx
+++ src/components/molecules/Form.tsx
@@ -1,19 +1,28 @@
 import { Button } from "@mui/material";
 import { CustomTextField } from "../atoms/TextField";
 import { ImageInput } from "../atoms/ImageInput";
+import { useState } from "react";
 
 export const Form = () => {
-  const titleChangeHandler = () => {};
-  const photoChangeHandler = () => {};
+  const [title, setTitle] = useState<string>("");
+  const [photo, setPhoto] = useState<FileList | null>(null);
+
+  const titleChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+  const photoChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.files);
+    setPhoto(e.target.files);
+  };
 
   return (
     <form className="flex flex-col">
       <CustomTextField
         label="What happen today"
-        value="value"
+        value={title}
         changeHandler={titleChangeHandler}
       />
-      <ImageInput imageChangeHandler={photoChangeHandler} />
+      <ImageInput imageChangeHandler={photoChangeHandler} value={photo} />
       <Button>Submit</Button>
     </form>
   );
